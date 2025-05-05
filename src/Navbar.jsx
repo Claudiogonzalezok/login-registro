c17afcdfa80faa1f23bdf2d44652b9a8ae4ba5df
Claudio Gonzalez <188018680+Claudiogonzalezok@users.noreply.github.com>
2025-05-04 22:48:47 -0300

    este commit es para subir los archivos a git hub

---
+++ src/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Outlet, Link } from 'react-router-dom'
+
+export const Navbar = () => {
+  return (
+    <>
+        <header>
+          <h1>
+            <Link to='/'>Logo</Link>
+          </h1>
+
+          <nav>
+            <Link to='/Login'>Iniciar Sesiòn</Link>
+            <Link to='/Register'>Registarse</Link>
+          </nav>
+          <div className="user">
+            <span className="username">Roberto</span>
+            <button className='btn-logout'>Cerrar Sesiòn</button>
+          </div>
+        </header>
+
+        <Outlet/>
+    </>
+  )
+}
